8215aa085186fa26360a3070959af2a7511d9d3c
Okpainmo <okpainmoandrew@gmail.com>
2026-05-30 12:11:53 +0100

    chore: static analysis...

---
+++ prettier.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import("prettier").Config} */
+const config = {
+    // Only applied when running against .md files
+    proseWrap: "always",
+    printWidth: 100,
+};
+
+export default config;
